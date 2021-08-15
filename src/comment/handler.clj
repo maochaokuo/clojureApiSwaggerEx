50d94194c548242ad9988f1c8b31b3f6571b95ba
Thomas Mao <tm1813@gmail.com>
2021-08-15 10:14:04 +0800

    add app test ring only

--- src/comment/handler.clj
+++ src/comment/handler.clj
@@ -5,21 +5,23 @@
 
 (def routes
   [["/ping" {:get (fn [req] {:status 200 :body "ok"})}] ; fn replace lamda char
-   ["/api"
-    ["/users" ::users]
-    ["/users/:id" ::user-id]
-    ["/posts" ::posts]]])
+   ])
 
 (def router
   (ring/router routes))
 
-(r/routes router)
+(def app
+  (ring/ring-handler router))
 
-(r/match-by-path router "/ping")
-(r/match-by-path router "/api/users/333")
-
-(r/match-by-name router ::ping)
+(app {:request-method :get :uri "/ping"})
 
 (comment
+  (r/routes router)
+
+  (r/match-by-path router "/ping")
+  (r/match-by-path router "/api/users/333")
+
+  (r/match-by-name router ::ping)
+
   (r/routes router)
   )
