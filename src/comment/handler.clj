d95a19977c121596f26777b32e6d13ceb3642e78
Thomas Mao <tm1813@gmail.com>
2021-08-15 18:02:45 +0800

    end of 1st video

--- src/comment/handler.clj
+++ src/comment/handler.clj
@@ -1,54 +1,57 @@
-(ns comment.handler 
+(ns comment.handler
   (:require
-   [reitit.core :as r]
-   [reitit.ring :as ring]
-   [reitit.swagger :as swagger]
-   [reitit.swagger-ui :as swagger-ui]
-   [reitit.ring.middleware.muuntaja :as muuntaja]
-   [reitit.ring.middleware.exception :as exception]
-   [reitit.coercion.spec]
-   [reitit.ring.coercion :as coercion]
-   [muuntaja.core :as m]
-   [ring.adapter.jetty :as jetty]))
+    [reitit.core :as r]
+    [reitit.ring :as ring]
+    [reitit.swagger :as swagger]
+    [reitit.swagger-ui :as swagger-ui]
+    [reitit.ring.middleware.muuntaja :as muuntaja]
+    [reitit.ring.middleware.exception :as exception]
+    [reitit.coercion.spec]
+    [reitit.ring.coercion :as coercion]
+    [muuntaja.core :as m]
+    [ring.adapter.jetty :as jetty]))
 
 (def ok (constantly {:status 200 :body "ok"}))
 
 (def routes
   [["/swagger.json"
-    {:get {:no-doc true
+    {:get {:no-doc  true
            :swagger {:info {:title "Comment System API"}}
            :handler (swagger/create-swagger-handler)}}]
    ["/comments"
     {:swagger {:tags ["comments"]}}
     [""
-     {:get {:summary "Get all comments"
-            :handler ok}
-      :post {:summary "Create a new comment"
-             :parameters {:body {:name string?
-                                 :slug string?
-                                 :text string?
+     {:get  {:summary "Get all comments"
+             :handler ok}
+      :post {:summary    "Create a new comment"
+             :parameters {:body {:name              string?
+                                 :slug              string?
+                                 :text              string?
                                  :parent-comment-id int?}}
-             :responses {200 {:body string?}}
-             :handler ok}}]
+             :responses  {200 {:body string?}}
+             :handler    ok}}]
     ["/:slug"
-     {:get {:summary "Get comments by slug"
+     {:get {:summary    "Get comments by slug"
             :parameters {:path {:slug string?}}
-            :handler ok}}]
+            :handler    ok}}]
     ["/id/:id"
-     {:put {:summary "Update a comment by the moderator"
-            :parameters {:path {:id int?}}
-            :handler ok}
-      :delete {:summary "Delete a comment by the moderator"
+     {:put    {:summary    "Update a comment by the moderator"
                :parameters {:path {:id int?}}
-               :handler ok}}]]
+               :handler    ok}
+      :delete {:summary    "Delete a comment by the moderator"
+               :parameters {:path {:id int?}}
+               :handler    ok}}]]
    ])
 
 (def router
   (ring/router routes
-               {:data {:coercion reitit.coercion.spec/coercion
-                       :muuntaja m/instance
-                       :middleware [muuntaja/format-middleware
+               {:data {:coercion   reitit.coercion.spec/coercion
+                       :muuntaja   m/instance
+                       :middleware [swagger/swagger-feature
+                                    muuntaja/format-negotiate-middleware
+                                    muuntaja/format-response-middleware
                                     exception/exception-middleware
+                                    muuntaja/format-request-middleware
                                     coercion/coerce-request-middleware
                                     coercion/coerce-response-middleware
                                     ]}}))
